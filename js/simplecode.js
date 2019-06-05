87f0aee4daf88a894a4f7452ea7270212ce30ed5
Patrick Gabbard <gabbbarddesigns@gmail.com>
2019-06-05 18:44:59 -0400

    added jquery

--- js/simplecode.js
+++ js/simplecode.js
@@ -1,11 +1,22 @@
 
-function randomNumber(upper) {
-    return Math.floor( Math.random()* upper) +1;
-};
+// function randomNumber(upper) {
+//     return Math.floor( Math.random()* upper) +1;
+// };
 
-var counter = 0;
-while (counter < 10) {
-    var randNum = randomNumber(6);
-    document.write(randNum + '');
-    counter+= +1;
-}
+// var counter = 0;
+// while (counter < 10) {
+//     var randNum = randomNumber(6);
+//     document.write(randNum + '');
+//     counter+= +1;
+// }
+
+
+$.getJSON('../data/inventory.json',function(data){
+console.log(data);
+var output = '<ul>';  
+$.each(data, function(key,val){
+  output += '<li>'+ val.name + " " + val.age+ '</li>';
+});
+output += '</ul>';
+$('#update').html(output);
+});
