8dea6ed1e110e64705711a5d708f06475161ffea
Patrick Gabbard <gabbbarddesigns@gmail.com>
2019-06-05 19:16:30 -0400

    initial json read

--- js/simplecode.js
+++ js/simplecode.js
@@ -17,5 +17,5 @@ var output ='';
 $.each(data, function(key,val){
     output += '<div class="product">'+'<p class="title">'+val.title+'</p>'+'<div class="image_line">'+'<img src="'+val.imagepath+'">'+'</div>'+'<p class="price">$'+val.price+'</p>'+'</div>';
 });
-$('#update').html(output);
+$('#inventory').html(output);
 });
