45929540c2ede1bcf74ddf9a9e5ad3b9662463c8
Patrick Gabbard <gabbbarddesigns@gmail.com>
2019-06-05 19:34:31 -0400

    second json read

--- js/simplecode.js
+++ js/simplecode.js
@@ -13,8 +13,9 @@
 
 $.getJSON('./data/inventory.json',function(data){
 console.log(data);
+var output ='';
 $.each(data, function(key,val){
     output += '<div class="product">'+'<p class="title">'+val.title+'</p>'+'<div class="image_line">'+'<img src="'+val.imagepath+'">'+'</div>'+'<p class="price">$'+val.price+'</p>'+'</div>';
 });
-$('#update').html(output);
+$('#inventory').html(output);
 });
