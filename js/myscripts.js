6761b3dad6778760d93c57ca827480c14eab9b1e
Patrick Gabbard <gabbbarddesigns@gmail.com>
2019-06-05 18:16:11 -0400

    next one

--- js/myscripts.js
+++ js/myscripts.js
@@ -22,14 +22,3 @@ function howto() {
     document.getElementById("main").classList.add("fadeout");
     setTimeout( function() { window.location = "howto.html" }, 3500 );
 };
-
-function randomNumber(upper) {
-    return Math.floor( Math.random()* upper) +1;
-};
-
-var counter = 0;
-while (counter < 10) {
-    var randNum = randomNumber(6);
-    document.write(randNum + '');
-    counter+= +1;
-}
