cac8f4f411df40524f03aa2819e379b385beac35
Patrick Gabbard <gabbbarddesigns@gmail.com>
2019-06-08 17:26:47 -0400

    second json read

--- js/simplecode.js
+++ js/simplecode.js
@@ -1,21 +1,18 @@
 
-// function randomNumber(upper) {
-//     return Math.floor( Math.random()* upper) +1;
-// };
-
-// var counter = 0;
-// while (counter < 10) {
-//     var randNum = randomNumber(6);
-//     document.write(randNum + '');
-//     counter+= +1;
-// }
-
+// Read JSON Datafile for starter inventory
 
 $.getJSON('./data/inventory.json',function(data){
 console.log(data);
 var output ='';
+var inventory_Array=[];
 $.each(data, function(key,val){
+    inventory_Array[key][0]= key;
+    inventory_Array[key][1]= val.title;
+    inventory_Array[key][2]= val.imagepath;
+    inventory_Array[key][3]= val.price;
     output += '<div class="product">'+'<p class="title">'+val.title+'</p>'+'<div class="image_line">'+'<img src="'+val.imagepath+'">'+'</div>'+'<p class="price">$'+val.price+'</p>'+'</div>';
 });
 $('#inventory').html(output);
 });
+
+console.log[inventory_Array[1][1]];
