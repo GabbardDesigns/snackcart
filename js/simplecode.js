8ae896e875d57bd28141fa89ada9d971a0d8372f
Patrick Gabbard <gabbbarddesigns@gmail.com>
2019-06-08 17:35:04 -0400

    second json read

--- js/simplecode.js
+++ js/simplecode.js
@@ -6,10 +6,7 @@ console.log(data);
 var output ='';
 var inventory_Array=[];
 $.each(data, function(key,val){
-    inventory_Array[key][0]= key;
-    inventory_Array[key][1]= val.title;
-    inventory_Array[key][2]= val.imagepath;
-    inventory_Array[key][3]= val.price;
+    inventory_Array.push( [key, val.title, val.imagepath, val.price]);
     output += '<div class="product">'+'<p class="title">'+val.title+'</p>'+'<div class="image_line">'+'<img src="'+val.imagepath+'">'+'</div>'+'<p class="price">$'+val.price+'</p>'+'</div>';
 });
 $('#inventory').html(output);
