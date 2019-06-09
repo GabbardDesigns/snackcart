ce528ee77ab90157dc61c5ea9dece1d3f34df521
Patrick Gabbard <gabbbarddesigns@gmail.com>
2019-06-09 11:53:45 -0400

    removed currency.js, added formatMoney script

--- js/simplecode.js
+++ js/simplecode.js
@@ -6,8 +6,22 @@ var output ='';
 var ordersblock ='';
 var price= parseFloat('0.00',10);
 
-// Removed until I can get currency working
-// var total_Price = currency(price);
+// formats currency 
+function formatMoney(amount, decimalCount = 2, decimal = ".", thousands = ",") {
+    try {
+      decimalCount = Math.abs(decimalCount);
+      decimalCount = isNaN(decimalCount) ? 2 : decimalCount;
+  
+      const negativeSign = amount < 0 ? "-" : "";
+  
+      let i = parseInt(amount = Math.abs(Number(amount) || 0).toFixed(decimalCount)).toString();
+      let j = (i.length > 3) ? i.length % 3 : 0;
+  
+      return negativeSign + (j ? i.substr(0, j) + thousands : '') + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thousands) + (decimalCount ? decimal + Math.abs(amount - i).toFixed(decimalCount).slice(2) : "");
+    } catch (e) {
+      console.log(e)
+    }
+  };
 
 // Clear initial arrays
 inventory_Array.pop(); inventory_Array.pop();
@@ -29,8 +43,7 @@ $('#inventory').html(output);
 
 function calculatePrice(priceAdd){
        price+= priceAdd;  
-       total_Price = currency(price);
-       $('#order_total').html(price); 
+       $('#order_total').html(formatMoney(price)); 
     };
 
 
@@ -50,7 +63,6 @@ function moveToCart(p1){
 function clearOrder(){
     ordersblock =''; 
     price= parseFloat('0.00');
-    total_Price = currency(price);
     $('#orders').html(ordersblock); 
     $('#order_total').html(price); 
 };
