9ee58ffc35076ac2991e68d02a6af50ec56481ce
Patrick Gabbard <gabbbarddesigns@gmail.com>
2019-06-10 14:40:43 -0400

    added redrawOrders function

--- js/simplecode.js
+++ js/simplecode.js
@@ -49,11 +49,12 @@ function calculatePrice(priceAdd){
 
 // Moves items to cart, calls calculatePrice to update price 
 function moveToCart(p1){
-    var splits= p1.split('-'); var mykey = parseInt(splits[1]); console.log(mykey);
+    var splits= p1.split('-'); 
+    var mykey = parseInt(splits[1]);
     var key =  parseInt((inventory_Array[mykey][0]),10);
     order_Array.push([key, inventory_Array[key][1], inventory_Array[key][2], inventory_Array[key][3]]);
     console.log(inventory_Array[key]);
-    redrawOrders();
+    //redrawOrders();
     calculatePrice(parseFloat((inventory_Array[key][3])));
 };
 
